8893d26c77c954684c6da234239f1c96ca237414
jlei40 <jlei40@stuy.edu>
2024-06-03 15:32:05 -0400

    almost done getting data in database

--- billboard/rankings/views.py
+++ billboard/rankings/views.py
@@ -8,6 +8,10 @@ from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
+import json
+import http.client
+from django.conf import settings
+from django.utils.dateparse import parse_date
 
 
 
@@ -29,3 +33,61 @@ def add_to_favorites(request, song_id):
         return JsonResponse({'status': 'added'})
     else:
         return JsonResponse({'status': 'not added'})
+
+def fetch_and_display_songs(request):
+    api_key = settings.API_KEY
+
+    conn = http.client.HTTPSConnection("billboard-api2.p.rapidapi.com")
+
+    headers = {
+        'x-rapidapi-key': api_key,
+        'x-rapidapi-host': "billboard-api2.p.rapidapi.com"
+    }
+
+    conn.request("GET", "/hot-100?date=2019-05-11&range=1-100", headers=headers)  # Placeholder date
+
+    res = conn.getresponse()
+    data = res.read()
+
+    songs = data.decode("utf-8")
+    songs_data = json.loads(songs)
+    print("API response:", songs_data)  # Add this line
+
+    song_list = songs_data.get('content', [])
+    print("Song list:", song_list)  # Add this line
+
+    def process_song_data(songs_data):
+        for song_info in songs_data:
+            title = song_info.get('title')
+            artist = song_info.get('artist')
+            rank = int(song_info.get('rank'))
+            peak_position = int(song_info.get('peak position'))
+            weeks_on_chart = int(song_info.get('weeks on chart'))
+
+            chart_date = parse_date('2019-05-11')  # Placeholder date
+
+            song, created = Song.objects.update_or_create(
+                title=title,
+                artist=artist,
+                chart_date=chart_date,
+                defaults={
+                    'rank': rank,
+                    'album_name': '',
+                    'peak_position': peak_position,
+                    'weeks_on_chart': weeks_on_chart,
+                    'label': '',
+                    'genre': ''
+                }
+            )
+            if created:
+                print(f"Added new song: {title} by {artist}")
+            else:
+                print(f"Updated song: {title} by {artist}")
+
+    process_song_data(song_list)
+
+    # Fetch the songs from the database to pass to the template
+    songs = Song.objects.filter(chart_date='2019-05-11')  # Use the same date as above
+    print(f"Number of songs: {songs.count()}")  # Add this line
+
+    return render(request, 'song_list.html', {'songs': songs})
